9987a83334ddf61cfb4a64649cfd148378c7dc3e
Kr328 <39107975+Kr328@users.noreply.github.com>
2019-11-19 09:30:41 +0800

    use kotlin for core

--- core/src/main/java/com/github/kr328/clash/core/Clash.java
+++ core/src/main/java/com/github/kr328/clash/core/Clash.java
@@ -1,14 +1,9 @@
 package com.github.kr328.clash.core;
 
-public final class Clash {
-    public static native void init(String clashLibrary, String home);
-    public static native void loadDefault();
-    public static native void loadProfileFromPath(String path) throws ClashException;
-    public static native void startTun(int fd, int mtu) throws ClashException;
-    public static native void stopTun() throws ClashException;
+import android.content.Context;
 
-    static {
-        System.loadLibrary("clash");
-        System.loadLibrary("bridge");
+public final class Clash {
+    public Clash(Context context) {
+
     }
 }
--- core/src/main/java/com/github/kr328/clash/core/ClashException.java
+++
@@ -1,7 +0,0 @@
-package com.github.kr328.clash.core;
-
-public class ClashException extends RuntimeException {
-    public ClashException(String message) {
-        super(message);
-    }
-}
