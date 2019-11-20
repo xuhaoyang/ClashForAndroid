96b8fe110bb75eaeca14b907ad7ed688b85048d7
Kr328 <39107975+Kr328@users.noreply.github.com>
2019-11-21 00:15:00 +0800

    MainActivity

--- app/src/main/java/com/github/kr328/clash/MainActivity.kt
+++ app/src/main/java/com/github/kr328/clash/MainActivity.kt
@@ -2,10 +2,15 @@ package com.github.kr328.clash
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(activity_main_toolbar.apply {
+            setLogo(R.mipmap.ic_launcher_foreground)
+        })
     }
 }
