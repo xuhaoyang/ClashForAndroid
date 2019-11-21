765892e7682422ae1cdf4cf8d46ccca01bd54a63
Kr328 <39107975+Kr328@users.noreply.github.com>
2019-11-22 00:05:23 +0800

    set default state to stopped

--- app/src/main/java/com/github/kr328/clash/MainActivity.kt
+++ app/src/main/java/com/github/kr328/clash/MainActivity.kt
@@ -59,6 +59,12 @@ class MainActivity : AppCompatActivity() {
             setLogo(R.mipmap.ic_launcher_foreground)
         })
 
+        activity_main_clash_status_icon.setImageResource(R.drawable.ic_clash_stopped)
+        activity_main_clash_status_title.text = getString(R.string.clash_status_stopped)
+        activity_main_clash_status_summary.text = getString(R.string.clash_status_click_to_start)
+        activity_main_clash_proxies.visibility = View.GONE
+        activity_main_clash_logs.visibility = View.GONE
+
         clashStatus.observe(this) {
             handler.removeMessages(0)
             handler.postDelayed({
