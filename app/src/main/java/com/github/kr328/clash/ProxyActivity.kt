aa4b6c23bbc7d34052f46223e6f05041cfe59849
Kr328 <39107975+Kr328@users.noreply.github.com>
2019-11-26 23:30:51 +0800

    proxy activity

--- app/src/main/java/com/github/kr328/clash/ProxyActivity.kt
+++ app/src/main/java/com/github/kr328/clash/ProxyActivity.kt
@@ -1,5 +1,19 @@
 package com.github.kr328.clash
 
-class ProxyActivity : BaseActivity() {
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
+class ProxyActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val content = RecyclerView(this)
+
+        setContentView(content)
+
+        content.layoutManager = LinearLayoutManager(this)
+
+        
+    }
 }
