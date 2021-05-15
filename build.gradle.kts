2f69985fe10c5bb9140427bcd303fc095ca00421
kr328 <kr328app@outlook.com>
2021-05-15 19:15:43 +0800

    Chore: update gradle wrapper & append sha256sum

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 @file:Suppress("UNUSED_VARIABLE")
 
+import java.net.URL
+
 allprojects {
     repositories {
         google()
@@ -13,4 +15,16 @@ allprojects {
 
 task("clean", type = Delete::class) {
     delete(rootProject.buildDir)
+}
+
+tasks.named<Wrapper>("wrapper") {
+    distributionType = Wrapper.DistributionType.ALL
+
+    doLast {
+        val sha256 = URL("$distributionUrl.sha256").openStream()
+            .use { it.reader().readText().trim() }
+
+        file("gradle/wrapper/gradle-wrapper.properties")
+            .appendText("distributionSha256Sum=$sha256")
+    }
 }
