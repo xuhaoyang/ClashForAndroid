e7fef0a767e1f471797621b018aa79d23d202533
kr328 <kr328app@outlook.com>
2021-05-25 22:30:38 +0800

    Chore: bump version

--- buildSrc/src/main/java/Build.kt
+++ buildSrc/src/main/java/Build.kt
@@ -1,7 +1,7 @@
 import org.gradle.api.Project
 
-const val buildVersionCode = 204003
-const val buildVersionName = "2.4.3"
+const val buildVersionCode = 204004
+const val buildVersionName = "2.4.4"
 
 const val buildMinSdkVersion = 21
 const val buildTargetSdkVersion = 30
