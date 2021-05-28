428ca53532ff849a99c8c533b534bf910d1c4a48
Goooler <wangzongler@gmail.com>
2021-05-28 10:37:23 +0800

    Chore: use "implementation" & remove "exclude" for buildSrc dependencies (#1034)

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,20 +9,10 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib"))
-
-    compileOnly(gradleApi())
-
-    api(kotlin("gradle-plugin"))
-    api(kotlin("serialization"))
-    api("com.android.tools.build:gradle:4.2.1") {
-        exclude("org.jetbrains.kotlin", "kotlin-stdlib-jdk8")
-        exclude("org.jetbrains.kotlin", "kotlin-stdlib-jdk7")
-        exclude("org.jetbrains.kotlin", "kotlin-reflect")
-    }
-    api("com.google.devtools.ksp:symbol-processing-gradle-plugin:1.5.0-1.0.0-alpha10") {
-        exclude("com.android.tools.build", "gradle")
-    }
+    implementation(kotlin("gradle-plugin"))
+    implementation(kotlin("serialization"))
+    implementation("com.android.tools.build:gradle:4.2.1")
+    implementation("com.google.devtools.ksp:symbol-processing-gradle-plugin:1.5.0-1.0.0-alpha10")
 }
 
 gradlePlugin {
