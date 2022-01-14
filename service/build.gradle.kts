12c704d89cce62dcdbd6878f15776e2ce4f82963
Kr328 <kr328app@outlook.com>
2022-01-14 23:40:41 +0800

    Chore: reformat build.gradle.kts

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -7,11 +7,11 @@ plugins {
 }
 
 dependencies {
-    ksp(deps.kaidl.compiler)
-    kapt(deps.androidx.room.compiler)
-
     implementation(project(":core"))
     implementation(project(":common"))
+
+    ksp(deps.kaidl.compiler)
+    kapt(deps.androidx.room.compiler)
 
     implementation(deps.kotlin.coroutine)
     implementation(deps.kotlin.serialization.json)
