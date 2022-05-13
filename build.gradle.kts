ffa559a57102ed8bc20eba41aa236a7764741bf9
kr328 <kr328app@outlook.com>
2022-05-13 13:50:39 +0800

    Chore: bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,8 +40,8 @@ subprojects {
             minSdk = 21
             targetSdk = 31
 
-            versionName = "2.5.8"
-            versionCode = 205008
+            versionName = "2.5.9"
+            versionCode = 205009
 
             resValue("string", "release_name", "v$versionName")
             resValue("integer", "release_code", "$versionCode")
